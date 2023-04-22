ca2c07f3bef60572a94bca3f3541006713cb3ea7
Freelife05 <83334140+Freelife05@users.noreply.github.com>
2023-04-23 00:00:17 +0300

    Database Added

--- FiremanSystem/FiremanSystem/FiremanSystem/Program.cs
+++ FiremanSystem/FiremanSystem/FiremanSystem/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FiremanSystem.View;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +17,7 @@ namespace FiremanSystem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new LoginView());
         }
     }
 }
